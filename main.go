d1ae0f245cfc0ef42de3d7712116e0a3ee8db087
whoopii <woohyeon88@daolcompany.com>
2024-07-19 00:45:49 +0900

    first block chain

--- main.go
+++ main.go
@@ -15,14 +15,29 @@ type blockchain struct {
 	blocks []block
 }
 
-func (b *blockchain) addBlock(data string){
-	newBlock := block{data, "", ""}
-	if len(block)
-
-
+func (b *blockchain) getLastHash() string {
+	return b.blocks[len(b.blocks)-1].hash
 }
 
-func (b *blockchain) listBlocks(){}
+func (b *blockchain) addBlock(data string){
+	newBlock := block{data, "", ""}
+	if len(b.blocks) > 0 {
+		// hash값이 있어야함
+		newBlock.prevHash = b.getLastHash()
+	}else {
+		newBlock.prevHash = ""
+	}
+	newBlock.hash = fmt.Sprintf("%x", sha256.Sum256([]byte(data + newBlock.prevHash)))
+	b.blocks = append(b.blocks, newBlock)
+}
+
+func (b *blockchain) listBlocks(){
+	for _, block := range b.blocks {
+		fmt.Printf("Data: %s\n", block.data)
+		fmt.Printf("Hash: %s\n", block.hash)
+		fmt.Printf("PrevHash: %s\n\n", block.prevHash)
+	}
+}
 
 func main() {
 	genesisBlock := block{
