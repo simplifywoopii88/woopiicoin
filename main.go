71b0362f78ec13cdd92294f144bbf6eaa4f441bf
whoopii <woohyeon88@daolcompany.com>
2024-07-28 21:46:07 +0900

    HTML 추가

--- main.go
+++ main.go
@@ -1,21 +1,12 @@
 package main
 
-import (
-	"fmt"
+import "github.com/simplifywoopii88/woopiicoin/explorer"
 
-	"github.com/simplifywoopii88/woopiicoin/blockchain"
-)
+
+
+
 
 
 func main() {
-	chain := blockchain.GetBlockchain()
-	chain.AddBlock("Second Block")
-	chain.AddBlock("Third Block")
-	chain.AddBlock("Fourth Block")
-	for _, block := range chain.AllBlocks() {
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %s\n", block.Hash)
-		fmt.Printf("PrevHash: %s\n\n", block.PrevHash)
-	}
-	
+	explorer.Start()
 }
